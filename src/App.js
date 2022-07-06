7072bced35e44dad09ecfe309eadb2fc1ae46cba
imakish <makishtp@gmail.com>
2022-07-06 11:30:15 +0530

    Initially check outed the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Whatis from './components/Whatis';
-import Testimonial from './components/Testimonial';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Whatis from "./components/Whatis";
+import Testimonial from "./components/Testimonial";
 // import Slider from 'react-slick';
-import Sliders from './components/Slider';
-import Whyus from './components/Whyus';
-import Faq from './components/Faq';
-import End from './components/End';
+import Sliders from "./components/Slider";
+import Whyus from "./components/Whyus";
+import Faq from "./components/Faq";
+import End from "./components/End";
 
 function App() {
   return (
-    <div className=" overflow-hidden" style={{fontFamily:"Poppins"}}>
-      <Header/>
-      <Banner/>
-      <Whatis/>
-      <Testimonial/>
-      <Sliders/>
-      <Whyus/>
-      <Faq/>
-      <End/>
+    <div className=" overflow-hidden" style={{ fontFamily: "Poppins" }}>
+      <Header />
+      <Banner />
+      <Whatis />
+      <Testimonial />
+      <Sliders />
+      <Whyus />
+      <Faq />
+      <End />
     </div>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,51 +12,26 @@ function Banner() {
     <div style={{ height: "768px" }}>
       {/* <div style={{backgroungImage:" linear-gradient(90deg, rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0) 72.53%), url(https://cdn.t.shef.com/unsafe/2400x0/center/middle/https://cdn.shef.com/static/media/bas_hero_d.dbb55085.jpg);"
         }}>hi</div> */}
-      <div className='relative w-full h-full'>
+      <div className="relative w-full h-full">
         <img
-          src='/assets/banner.webp'
-          className='w-full h-full brightness-75 object-cover '
+          src="/assets/banner.webp"
+          className="w-full h-full brightness-75 object-cover "
         />
-        <div className='absolute top-10 md:top-40 w-11/12 md:w-4/5 bg '>
-          <p className='text-4xl md:text-6xl xl:text-7xl text-white font-bold m-6 md:px-28'>
+        <div className="absolute top-10 md:top-40 w-11/12 md:w-4/5 bg ">
+          <p className="text-4xl md:text-6xl xl:text-7xl text-white font-bold m-6 md:px-28">
             Earn money doing what you love
           </p>
-          <p className='text-xl md:text-3xl lg:text-3xl text-white mx-6 md:px-28'>
+          <p className="text-xl md:text-3xl lg:text-3xl text-white mx-6 md:px-28">
             Sign up to be your own boss and cook whenever you want.
           </p>
-          <div className='md:flex m-8 items-center'>
+          <div className="md:flex m-8 items-center">
             <input
-              type='text'
-              placeholder='Phone number'
-              className='py-2 md:ml-28 mr-6 md:px-6 my-2 h-16 w-full rounded outline-none'
+              type="text"
+              placeholder="Phone number"
+              className="py-1 md:ml-28 mr-6 md:px-6 my-2 h-12 w-2/4 rounded outline-none"
             />
-            <div className='flex-col'>
-              <div className='my-2'>
-                <input
-                  type='checkbox'
-                  name='chk1'
-                  id=''
-                  className='p-4 w-4 h-4'
-                />
-                <label for='chk1' className='text-white md:text-lg mx-2'>
-                  I am an Afghan refugee applying for expedited review
-                </label>
-                <br />
-              </div>
-              <div className='my-2'>
-                <input
-                  type='checkbox'
-                  name='chk1'
-                  id=''
-                  className='p-4 w-4 h-4 '
-                />
-                <label for='chk1' className='text-white md:text-lg mx-2'>
-                  I agree to Shef's Terms of Service & Privacy Policy.
-                </label>
-              </div>
-            </div>
           </div>
-          <p className='text-xl text-white p-2 px-9    rounded-3xl mx-8 md:mx-36 my-12 bg-[#d93A4a] w-fit'>
+          <p className="text-xl text-white p-2 px-9    rounded-3xl mx-8 md:mx-36 my-12 bg-[#d93A4a] w-fit">
             Get Started
           </p>
         </div>
